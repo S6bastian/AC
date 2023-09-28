d272ef581b9c3c05b2bc7422ed52b373d526ae3f
Sebastián Sánchez <seb2001sac@gmail.com>
2023-09-27 22:21:46 -0500

    v0.0.3 añadido float convertirBitsetAFloat(const bitset<32>& bits)

--- LAB02b/multiplicador_Punto_Flotante.cpp
+++ LAB02b/multiplicador_Punto_Flotante.cpp
@@ -2,15 +2,19 @@
 #include <bitset>
 using namespace std;
 
+// Función para convertir un bitset de 32 bits a float
+float convertirBitsetAFloat(const bitset<32>& bits) {
+    uint32_t valorEntero = bits.to_ulong();
+    return *reinterpret_cast<float*>(&valorEntero);
+}
+
 int main() {
-    float fx = 2.5; // Valor float
+    float fx = 2.5;
     float fy = 5.25;
 
-    // Convierte el valor float a un número entero sin signo de 32 bits
     uint32_t castx = *reinterpret_cast<uint32_t*>(&fx);
     uint32_t casty = *reinterpret_cast<uint32_t*>(&fy);
 
-    // Crea un objeto bitset a partir del valor entero
     bitset<32> fpx(castx);
     bitset<32> fpy(casty);
 
@@ -18,6 +22,13 @@ int main() {
     cout << "Representación binaria de 32 bits: " << fpx << endl;
     cout << "Valor float: " << fy << endl;
     cout << "Representación binaria de 32 bits: " << fpy << endl;
+
+    // Convertir los bitsets nuevamente a float
+    float convertido_x = convertirBitsetAFloat(fpx);
+    float convertido_y = convertirBitsetAFloat(fpy);
+
+    cout << "Convertido de binario a float (x): " << convertido_x << endl;
+    cout << "Convertido de binario a float (y): " << convertido_y << endl;
 
     return 0;
 }
