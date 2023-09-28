3e45293d5a6dd353a84b65b0a6cec7f3ccba9a09
Sebastián Sánchez <seb2001sac@gmail.com>
2023-09-27 21:53:22 -0500

    v0.0.1 fy añadido

--- LAB02b/multiplicador_Punto_Flotante.cpp
+++ LAB02b/multiplicador_Punto_Flotante.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main() {
-    float fx = -2.5; // Valor float
+    float fx = 2.5; // Valor float
     float fy = 3.25;
 
     // Convierte el valor float a un número entero sin signo de 32 bits
@@ -12,10 +12,12 @@ int main() {
 
     // Crea un objeto bitset a partir del valor entero
     bitset<32> fpx(castx);
+    bitset<32> fpy(castx);
 
-    cout << "Valor float: " << castx << endl;
     cout << "Valor float: " << fx << endl;
     cout << "Representación binaria de 32 bits: " << fpx << endl;
+    cout << "Valor float: " << fy << endl;
+    cout << "Representación binaria de 32 bits: " << fpy << endl;
 
     return 0;
 }
