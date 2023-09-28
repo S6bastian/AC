de829402e6ab92c95aff03ceb3deae58aed5d529
Sebastián Sánchez <seb2001sac@gmail.com>
2023-09-27 22:48:21 -0500

    V0.1.0 funciones agregadas

--- LAB02b/multiplicador_Punto_Flotante.cpp
+++ LAB02b/multiplicador_Punto_Flotante.cpp
@@ -2,6 +2,42 @@
 #include <bitset>
 using namespace std;
 
+// Función para sumar dos bitsets de 32 bits y devolver el resultado
+bitset<32> sumarBitsets(const bitset<32>& bits1, const bitset<32>& bits2) {
+    bitset<32> resultado;
+    bool carry = false;
+
+    for (int i = 31; i >= 0; --i) {
+        bool bit1 = bits1[i];
+        bool bit2 = bits2[i];
+
+        // Realizar la suma de bits considerando el acarreo
+        bool suma = bit1 ^ bit2 ^ carry;
+        
+        // Calcular el nuevo acarreo
+        carry = (bit1 & bit2) | (carry & (bit1 ^ bit2));
+
+        // Establecer el resultado en el bit correspondiente
+        resultado[i] = suma;
+    }
+
+    return resultado;
+}
+
+// Función para multiplicar dos bitsets de 32 bits y devolver el resultado
+bitset<32> multiplicarBitsets(const bitset<32>& bits1, const bitset<32>& bits2) {
+    bitset<32> resultado(0);
+
+    for (int i = 0; i < 32; ++i) {
+        if (bits2[i]) {
+            // Si el bit en bits2 es 1, suma bits1 al resultado desplazado
+            resultado ^= (bits1 << i);
+        }
+    }
+
+    return resultado;
+}
+
 // Función para convertir un bitset de 32 bits a float
 float convertirBitsetAFloat(const bitset<32>& bits) {
     uint32_t valorEntero = bits.to_ulong();
@@ -29,6 +65,20 @@ int main() {
 
     cout << "Convertido de binario a float (x): " << convertido_x << endl;
     cout << "Convertido de binario a float (y): " << convertido_y << endl;
+
+    // Sumar los bitsets
+    bitset<32> resultadoSuma = sumarBitsets(fpx, fpy);
+
+    cout << "Suma de las representaciones binarias: " << resultadoSuma << endl;
+    float c_resultadoSuma = convertirBitsetAFloat(resultadoSuma);
+    cout << "Convertido de binario a float (): " << c_resultadoSuma << endl;
+
+    // Multiplicar los bitsets
+    bitset<32> resultadoMultiplicacion = multiplicarBitsets(fpx, fpy);
+
+    cout << "Multiplicación de las representaciones binarias: " << resultadoMultiplicacion << endl;
+    float c_resultadoMultiplicacion = convertirBitsetAFloat(resultadoMultiplicacion);
+    cout << "Convertido de binario a float (): " << c_resultadoMultiplicacion << endl;
 
     return 0;
 }
