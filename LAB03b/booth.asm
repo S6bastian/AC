14c74ec5bd2e1f9c82ba778a205d17832e1d9c2f
Sebastián Sánchez <seb2001sac@gmail.com>
2023-10-13 21:15:23 -0500

    v1.0.1

--- LAB03b/booth.asm
+++ LAB03b/booth.asm
@@ -5,38 +5,38 @@ zero:   .word   0
 .globl  main
 main:
     # Inicializa los registros y la multiplicación de 64 bits
-    li $v0, 0        # Inicializa el registro de resultado (parte baja)
-    li $v1, 0        # Inicializa el registro de resultado (parte alta)
-    li $t0, 32       # Inicializa el contador para 32 bits
-    lw $t2, neg1     # Carga -1 en $t2 para usarlo en la comparación
+    li $v0, 0       
+    li $v1, 0        
+    li $t0, 32       
+    lw $t2, neg1     
 
 loop:
-    sll $t3, $a1, 31   # Obtiene el bit más significativo de $a1
-    sra $t4, $a0, 31   # Obtiene el bit más significativo de $a0
-    add $t3, $t3, $t4  # Comprueba los dos bits más significativos
+    sll $t3, $a1, 31   
+    sra $t4, $a0, 31   
+    add $t3, $t3, $t4  
 
     # Multiplica por 2 y suma/substrae
-    sllv $t5, $a0, $t0  # $t5 = $a0 << $t0
-    srav $t6, $a0, $t0  # $t6 = $a0 >> $t0
-    sub $t7, $t5, $t6   # $t7 = ($a0 << $t0) - ($a0 >> $t0)
+    sllv $t5, $a0, $t0 
+    srav $t6, $a0, $t0 
+    sub $t7, $t5, $t6  
 
     beq $t3, $t2, shift_left
     bne $t3, $zero, shift_right
 
 shift_right:
-    addu $v0, $v0, $t6  # $v0 += ($a0 >> $t0)
-    addu $v1, $v1, $t5  # $v1 += ($a0 << $t0)
+    addu $v0, $v0, $t6  
+    addu $v1, $v1, $t5  
 
     j next_iteration
 
 shift_left:
-    subu $v0, $v0, $t7  # $v0 -= ($a0 << $t0)
-    addu $v1, $v1, $t5  # $v1 += ($a0 << $t0)
+    subu $v0, $v0, $t7  
+    addu $v1, $v1, $t5  
 
 next_iteration:
-    srl $a1, $a1, 1    # Desplaza $a1 a la derecha
-    sll $a0, $a0, 1    # Desplaza $a0 a la izquierda
-    subi $t0, $t0, 1    # Decrementa el contador
-    bnez $t0, loop      # Si $t0 != 0, vuelve al bucle
+    srl $a1, $a1, 1    
+    sll $a0, $a0, 1   
+    subi $t0, $t0, 1    
+    bnez $t0, loop      
 
     jr $ra
